68a883616febe8b02040335df93d7e159287645f
Kevin Souza <kevinwillyansouza@gmail.com>
2022-10-20 10:15:23 -0400

    trabalhando...

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,1 +1,24 @@
-console.log('Olá Mundo');
+const conteinerEpisode = document.querySelector('.episodios-container');
+
+
+// numEpisode - Significa a quantidade de Ep do Anime
+var numEpisode = 13;
+var allLinksEpisode = [];
+
+const generationLinks = () => {
+	for(var i = 1; i <= numEpisode; i++){
+		var useNum = i
+		if(i < 10){
+			useNum = `0${i}`
+		}
+		allLinksEpisode.push(
+			`https://betteranime.net/anime/dublado/bastard-ankoku-no-hakaishin-ona-dublado/episodio-${useNum}`
+		)
+		// document.write(useNum + '<br>')
+	}
+}
+generationLinks()
+
+allLinksEpisode.forEach((episode, i) => {
+    conteinerEpisode.innerHTML += `<a> href="${episode}"Episódio ${i}<a>`
+})
